79f6f12d9bcc2a388e8a6f056e86480d7c99c842
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2025-05-12 11:52:17 -0300

    funcion para clonar cubos hecho

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClonarCubo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+    }
+    public void ClonarCubo()
+    {
+        Instantiate(cubePrefab);
+    }
+}
